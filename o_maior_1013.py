37fac447eb1c9c58ccc47579aab460169e3e61a4
codewithbea <portella.bea@icloud.com>
2022-06-12 18:07:50 -0300

    agora é o ultimo

--- o_maior_1013.py
+++ o_maior_1013.py
@@ -1,19 +1,18 @@
 # Desafio número 1013: "O Maior"
 
+import math
+
 primeiro_val_str, segundo_val_str, terceiro_val_str = input().split(" ")
 
-p_val = int(primeiro_val_str)
-s_val = int(segundo_val_str)
-t_val = int(terceiro_val_str)
+A = int(primeiro_val_str)
+B = int(segundo_val_str)
+C = int(terceiro_val_str)
 
-def formula_do_maior_valor():
-    if(p_val > s_val and p_val > t_val):
-        print('{} eh o maior'.format(p_val))
-    elif(s_val > p_val and s_val > t_val):
-        print('{} eh o maio'.format(s_val))
-    elif(t_val > p_val and t_val > s_val):
-        print('{} eh o maior'.format(t_val))
+def formula_maior_valor():
+    maior_AB = (A + B + abs(A - B))/2
+    resultado_final = (maior_AB + C + abs(maior_AB - C))/2
+    print("{:.0f} eh o maior".format(resultado_final))
 
-formula_do_maior_valor()
+formula_maior_valor()
 
 

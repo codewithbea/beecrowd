0c07eb846f80e4d39c7a8f5e5e2fcdfc050c6813
codewithbea <portella.bea@icloud.com>
2022-06-13 19:52:44 -0300

    outra versão de notas e moedas

--- notas_e_moedas_v2.py
+++ notas_e_moedas_v2.py
@@ -1,3 +1,7 @@
+#Desafio 1021: "Notas e moedas"
+#Continuou dando o mesmo erro: Wrong Answer (0%)
+
+
 
 valor_inserido = float(input())
 lista_notas = [100, 50, 20, 10, 5, 2]
@@ -11,10 +15,11 @@ def new_func(lista_notas, n):
 for n in range (0,6):
     valor_nota = new_func(lista_notas, n)
     num_de_notas = valor_inserido//valor_nota
-    print("{:.0f} nota(s) de R$ {:.0f}.00".format(num_de_notas, valor_nota))
+    print("{:.0f} nota(s) de R$ {:.2f}".format(num_de_notas, valor_nota))
     valor_inserido = valor_inserido - (valor_nota * num_de_notas)
 
-lista_moedas = [1, 0.5, 0.25, 0.10, 0.05, 0.01]
+
+lista_moedas = [1, 0.5, 0.25, 0.10, 0.05, 0.009] #Gambiarra porque com a moeda de 1 centavo dava erro por causa das divisões // 
 print("MOEDAS:")
 
 def new_func(lista_moedas, n):
@@ -24,5 +29,5 @@ def new_func(lista_moedas, n):
 for n in range (0,6):
     valor_moeda = new_func(lista_moedas, n)
     num_de_moedas = valor_inserido//valor_moeda
-    print("{:.0f} moedas(s) de R$ {:.2f}".format(num_de_moedas, valor_moeda))
+    print("{:.0f} moeda(s) de R$ {:.2f}".format(num_de_moedas, valor_moeda))
     valor_inserido = valor_inserido - (valor_moeda * num_de_moedas)
